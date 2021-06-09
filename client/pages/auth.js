bea7c5c3b8b09b09d9aca4770a1ea116bef71a5d
therealrinku <backupfb2032@gmail.com>
2021-06-09 12:13:59 +0545

    auth page design done

--- client/pages/auth.js
+++ client/pages/auth.js
@@ -2,15 +2,22 @@ import styles from "../styles/Authpage.module.css";
 import Link from "next/link";
 import { VscNote } from "react-icons/vsc";
 import { FiArrowRight } from "react-icons/fi";
+import { useState } from "react";
 
 export default function auth() {
+  const [loginMode, setLoginMode] = useState(true);
+
+  //form handlers;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className={styles.authpage}>
       <span>
         <VscNote />
         <Link href="/">BookStore</Link>
       </span>
-      <h2>Create a BookStore account</h2>
+      <h2>{loginMode ? "Login to BookStore" : "Create a BookStore account"}</h2>
 
       <form>
         <label for="email">Email Address</label>
@@ -18,13 +25,15 @@ export default function auth() {
         <label for="password">Password</label>
         <input type="password" id="password" placeholder="*****" />
         <button>
-          <p>Sign Up</p>
+          <p>{loginMode ? "Login" : "Sign Up"}</p>
           <FiArrowRight />
         </button>
       </form>
 
       <p style={{ margin: "25px 0 -10px 0", color: "gray" }}>Already have an account?</p>
-      <button style={{ border: "none" }}>Login to BookStore</button>
+      <button style={{ border: "none" }} onClick={() => setLoginMode((prev) => !prev)}>
+        {!loginMode ? "Login" : "Signup"} to BookStore
+      </button>
     </div>
   );
 }
