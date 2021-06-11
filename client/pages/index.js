b045c46b6c26fce04ef924daab9bf330d40a0700
therealrinku <backupfb2032@gmail.com>
2021-06-11 12:30:34 +0545

    homepage changed to landingpage

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,11 @@
-import styles from "../styles/Homepage.module.css";
+import styles from "../styles/LandingPage.module.css";
 import { useRouter } from "next/router";
 
-export default function Homepage() {
+export default function LandingPage() {
   const router = useRouter();
 
   return (
-    <div className={styles.homepage}>
+    <div className={styles.landingPage}>
       <img src="https://bit.ly/3zfmByz" alt="hero-image" />
       <section>
         <h3>Read Free Books only on book store</h3>
