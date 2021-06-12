5127bde39e97daf8c26ab13b0d0f9f02891a7667
therealrinku <backupfb2032@gmail.com>
2021-06-12 15:20:57 +0545

    getting books done

--- client/pages/home.js
+++ client/pages/home.js
@@ -1,40 +1,23 @@
 import styles from "../styles/Homepage.module.css";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BookForm from "../components/BookForm";
 import Backdrop from "../components/Backdrop";
+import axios from "axios";
+import apiUrl from "../apiUrl";
 
 export default function HomePage() {
   const [showBookForm, setShowBookForm] = useState(false);
+  const [books, setBooks] = useState([]);
 
-  const books = [
-    {
-      bookId: 1,
-      bookName: "Think and Grow Rich",
-      bookImage: "https://images-na.ssl-images-amazon.com/images/I/71EGWJeU3iL.jpg",
-    },
-    {
-      bookId: 2,
-      bookName: "How to win friends and influence people",
-      bookImage: "https://images-na.ssl-images-amazon.com/images/I/61bfGzdMr2L.jpg",
-    },
-    {
-      bookId: 3,
-      bookName: "Rich Dad Poor Dad",
-      bookImage:
-        "https://lh3.googleusercontent.com/proxy/TYNj9avpAaZtjlLRfm7-83c2XU9KwrxYV9Txe6bCr0nmkwlELKQLkx-PJPzDgm8_KJX6OTyjJRmbaP3ARXXAM8o4yjX8SySPTAvLiHH98whrZPGrpgH_iGQT5TYaHSs",
-    },
-    {
-      bookId: 4,
-      bookName: "Power of Now",
-      bookImage: "https://upload.wikimedia.org/wikipedia/en/6/66/TPON_Cover_LG.jpg",
-    },
-    {
-      bookId: 5,
-      bookName: "The richest man in babylon",
-      bookImage: "https://www.whatyouwilllearn.com/wp-content/uploads/2017/02/The-Richest-Man-in-Babylon.jpg",
-    },
-  ];
+  useEffect(() => {
+    axios
+      .get(apiUrl + "/book/all")
+      .then((res) => {
+        setBooks(res.data);
+      })
+      .catch((err) => alert(err.message));
+  }, []);
 
   const toggleBookForm = () => {
     setShowBookForm((prev) => !prev);
@@ -59,10 +42,10 @@ export default function HomePage() {
         {books.map((book, i) => {
           return (
             <div key={i}>
-              <Link href={`/book/${book.bookId}`}>
-                <img src={book.bookImage} alt={book.bookName} />
+              <Link href={`/book/${book._id}`}>
+                <img src={book.imageURL} alt={book.title} />
               </Link>
-              <p>{book.bookName}</p>
+              <p>{book.title}</p>
             </div>
           );
         })}
