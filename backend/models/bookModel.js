6f612dc43fb1052c5c93d5992125e6237c7c7421
therealrinku <backupfb2032@gmail.com>
2021-06-12 13:09:12 +0545

    models created

---
+++ backend/models/bookModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    price: { type: String, required: true },
+    author: { type: String, required: true },
+    details: { type: String, required: true },
+    imageURL: { type: String, required: true },
+    publishedDate: { type: Date, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const bookModel = mongoose.model("books", bookSchema);
+
+module.exports = bookModel;
---
+++ backend/models/userModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userModel = mongoose.model("users", userSchema);
+
+module.exports = userModel;
