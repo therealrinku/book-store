9b64c54cc43a458ee8461e401696b24c33319adb
therealrinku <backupfb2032@gmail.com>
2021-06-12 15:37:20 +0545

    loading view addeD

---
+++ client/components/LoadingView.js
@@ -0,0 +1,15 @@
+export default function LoadingView() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <p>Loading....</p>
+    </div>
+  );
+}
--- client/pages/home.js
+++ client/pages/home.js
@@ -5,18 +5,24 @@ import BookForm from "../components/BookForm";
 import Backdrop from "../components/Backdrop";
 import axios from "axios";
 import apiUrl from "../apiUrl";
+import LoadingView from "../components/LoadingView";
 
 export default function HomePage() {
   const [showBookForm, setShowBookForm] = useState(false);
   const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios
       .get(apiUrl + "/book/all")
       .then((res) => {
+        setLoading(false);
         setBooks(res.data);
       })
-      .catch((err) => alert(err.message));
+      .catch((err) => {
+        setLoading(false);
+        alert(err.message);
+      });
   }, []);
 
   const toggleBookForm = () => {
@@ -24,32 +30,38 @@ export default function HomePage() {
   };
 
   return (
-    <div className={styles.homepage}>
-      <p style={{ textAlign: "center", fontSize: "25px" }}>All Books</p>
+    <>
+      {loading ? (
+        <LoadingView />
+      ) : (
+        <div className={styles.homepage}>
+          <p style={{ textAlign: "center", fontSize: "25px" }}>All Books</p>
 
-      {showBookForm ? (
-        <>
-          <Backdrop toggle={toggleBookForm} />
-          <BookForm toggle={toggleBookForm} />
-        </>
-      ) : null}
+          {showBookForm ? (
+            <>
+              <Backdrop toggle={toggleBookForm} />
+              <BookForm toggle={toggleBookForm} />
+            </>
+          ) : null}
 
-      <button onClick={toggleBookForm} className={styles.AddBookButton}>
-        Add New Book
-      </button>
+          <button onClick={toggleBookForm} className={styles.AddBookButton}>
+            Add New Book
+          </button>
 
-      <section className={styles.books}>
-        {books.map((book, i) => {
-          return (
-            <div key={i}>
-              <Link href={`/book/${book._id}`}>
-                <img src={book.imageURL} alt={book.title} />
-              </Link>
-              <p>{book.title}</p>
-            </div>
-          );
-        })}
-      </section>
-    </div>
+          <section className={styles.books}>
+            {books.map((book, i) => {
+              return (
+                <div key={i}>
+                  <Link href={`/book/${book._id}`}>
+                    <img src={book.imageURL} alt={book.title} />
+                  </Link>
+                  <p>{book.title}</p>
+                </div>
+              );
+            })}
+          </section>
+        </div>
+      )}
+    </>
   );
 }
