f648636ab5edcbeda15532916388547071418dc8
therealrinku <backupfb2032@gmail.com>
2021-06-12 14:21:32 +0545

    mongodb connection done

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("connected to mongodb");
+});
 
 const app = express();
 
@@ -7,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 const authRoute = require("./routes/auth");
-const bookRoute = require("./route/book");
+const bookRoute = require("./routes/book");
 
 app.use("auth", authRoute);
 app.use("book", bookRoute);
