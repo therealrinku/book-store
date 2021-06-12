85b07e4416870847cef543090895d8753ef30349
therealrinku <backupfb2032@gmail.com>
2021-06-12 15:07:12 +0545

    input fields added

--- client/components/BookForm.js
+++ client/components/BookForm.js
@@ -1,15 +1,57 @@
+import { useState } from "react";
 import styles from "../styles/BookForm.module.css";
 
-export default function BookForm({ toggle }) {
+export default function BookForm({ toggle, editMode }) {
+  const [bookTitle, setBookTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishedDate, setPublishedDate] = useState("");
+  const [imageURL, setImageURL] = useState("");
+  const [price, setPrice] = useState("");
+  const [details, setDetails] = useState("");
+
+  const AddBook = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className={styles.BookForm}>
-      <form>
-        <input type="text" placeholder="Type book title here" />
-        <input type="text" placeholder="Type book author here" />
-        <input type="date" placeholder="Type book published date here" />
-        <input type="text" placeholder="Type book details here" />
-        <input type="text" placeholder="Type book image url here" />
-        <input type="text" placeholder="Type book price here" />
+      <form onSubmit={editMode ? null : AddBook}>
+        <input
+          type="text"
+          placeholder="Type book title here"
+          value={bookTitle}
+          onChange={(e) => setBookTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Type book author here"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <input
+          type="date"
+          placeholder="Type book published date here"
+          value={publishedDate}
+          onChange={(e) => setPublishedDate(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Type book details here"
+          value={details}
+          onChange={(e) => setDetails(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Type book image url here"
+          value={imageURL}
+          onChange={(e) => setImageURL(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Type book price here"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
         <button>Submit</button>
         <button onClick={toggle} type="button">
           Cancel
