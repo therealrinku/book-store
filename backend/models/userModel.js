e59ec1b6751f63db5eb3977b3a94e9d6a03c1a01
therealrinku <backupfb2032@gmail.com>
2021-06-12 17:57:13 +0545

    is admin field added

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema(
   {
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
   },
   {
     timestamps: true,
