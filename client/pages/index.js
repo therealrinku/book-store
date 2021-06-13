f6c4747781757e59dd0a8aece06292ea1c49c2fe
therealrinku <backupfb2032@gmail.com>
2021-06-13 13:39:48 +0545

    redirecting to homepage if login

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,27 @@
 import styles from "../styles/LandingPage.module.css";
 import { useRouter } from "next/router";
+import { useContext } from "react";
+import UserContext from "../UserContext";
+import HomePage from "./home";
 
 export default function LandingPage() {
   const router = useRouter();
 
+  const { userEmail } = useContext(UserContext);
+
   return (
-    <div className={styles.landingPage}>
-      <img src="https://bit.ly/3zfmByz" alt="hero-image" />
-      <section>
-        <h3>BOOK STORE</h3>
-        <button onClick={() => router.push("/auth")}>Login to get access</button>
-      </section>
-    </div>
+    <>
+      {userEmail ? (
+        <HomePage />
+      ) : (
+        <div className={styles.landingPage}>
+          <img src="https://bit.ly/3zfmByz" alt="hero-image" />
+          <section>
+            <h3>BOOK STORE</h3>
+            <button onClick={() => router.push("/auth")}>Login to get access</button>
+          </section>
+        </div>
+      )}
+    </>
   );
 }
